bf8af7d0b2a7188cf92a16ce663ceb78bcc6eaaa
Ajay Bhargav <contact@rickeyworld.info>
2021-10-04 12:00:23 +0530

    Add RDA8910 Support for Logicrom
    Signed-off-by: Ajay Bhargav <contact@rickeyworld.info>

--- platform.py
+++ platform.py
@@ -8,5 +8,16 @@ from platformio.managers.platform import PlatformBase
 
 class LogicromPlatform(PlatformBase):
     def configure_default_packages(self, variables, target):
+        
+        # configure script based on MCU type
+        board_config = self.board_config(variables.get("board"))
+        mcu = variables.get("board_build.mcu",
+                            board_config.get("build.mcu", "MT2503"))
+        if mcu not in ("MT2503", "MT6261", "MT2625"):
+            self.frameworks["logicromsdk"]["script"] = "builder/framework/%s/logicromsdk.py" % (
+                mcu.lower())
+            self.frameworks["arduino"]["script"] = "builder/framework/%s/arduino.py" % (
+                mcu.lower())
+
         return PlatformBase.configure_default_packages(self, variables,
                                                        target)
