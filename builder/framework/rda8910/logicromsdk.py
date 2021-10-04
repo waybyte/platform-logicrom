9b79217c3874c2f57e70f0298eeda565364d6cfa
Ajay Bhargav <contact@rickeyworld.info>
2021-10-04 11:58:44 +0530

    Add Logicrom SDK builder for RDA8910
    Signed-off-by: Ajay Bhargav <contact@rickeyworld.info>

---
+++ builder/framework/rda8910/logicromsdk.py
@@ -0,0 +1,212 @@
+# Copyright 2021 WAYBYTE Solutions
+#
+# SPDX-License-Identifier: MIT
+#
+
+from os.path import isdir, isfile, join
+from shutil import copyfile
+import json
+from platformio.util import get_systype
+
+from SCons.Script import DefaultEnvironment
+
+env = DefaultEnvironment()
+platform = env.PioPlatform()
+board = env.BoardConfig()
+
+FRAMEWORK_DIR = platform.get_package_dir("framework-logicromsdk")
+assert isdir(FRAMEWORK_DIR)
+
+# Load core configuration
+with open(join(FRAMEWORK_DIR, "lib", "rda8910", "core_config.json")) as f:
+    core_config = json.load(f)
+    f.close()
+
+# RDA Tools
+if "windows" in get_systype():
+    systype = "win32"
+else:
+    systype = "linux"
+
+tool_path = join(FRAMEWORK_DIR, "tools", "rda8910")
+
+env.Replace(
+    DTOOLS=join(tool_path, systype, "dtools"),
+    MKIMAGE='"$DTOOLS"' + " mkappimg",
+    PACGEN='"$PYTHONEXE" ' + join(tool_path, "pacgen.py"),
+    FOTAGEN='"$DTOOLS"' + " fotacreate2",
+)
+
+# Generate linker script
+linker_script = env.Command(
+    join("$BUILD_DIR", "linkerscript_out.ld"),
+    join(
+        FRAMEWORK_DIR, "lib", "rda8910", "app_flashimg.ld"
+    ),
+    env.VerboseAction(
+        '$CC -I"$FRAMEWORK_DIR/lib/rda8910" -P -x c -E $SOURCE -o $TARGET',
+        "Generating LD script $TARGET",
+    ),
+)
+env.Depends("$BUILD_DIR/$PROGNAME$PROGSUFFIX", linker_script)
+env.Replace(LDSCRIPT_PATH="linkerscript_out.ld")
+
+# Create Project Template
+main_c = join(env.subst("$PROJECT_DIR"), "src", "main.c")
+main_cpp = join(env.subst("$PROJECT_DIR"), "src", "main.cpp")
+if (False == isfile(main_c)) and (False == isfile(main_cpp)):
+    copyfile(join(FRAMEWORK_DIR, "template", "main.c"), main_c)
+
+def gen_pac_file(target, source, env):
+    (target_firm, ) = target
+    (source_elf, ) = source
+    # Generate image file
+    env.Execute(
+        env.VerboseAction("$MKIMAGE " + source_elf.get_abspath() + ' "$BUILD_DIR/$PROGNAME"' + '.img',
+        "Generating Firmware Image")
+    )
+
+    # Generate pac file
+    init_fdl = [
+        'cfg-init', '--pname', '"UIX8910_MODEM"', '--palias', '"$PROGNAME"',
+            '--pversion', '"8910 MODULE"', '--version', '"BP_R1.0.0"',
+            '--flashtype', '1',
+        'cfg-host-fdl', '-a', core_config["CONFIG_FDL1_IMAGE_START"], '-s', core_config["CONFIG_FDL1_IMAGE_SIZE"],
+            '-p', join(FRAMEWORK_DIR, 'lib', 'rda8910', 'fdl1.img'),
+        'cfg-fdl2', '-a', core_config["CONFIG_FDL2_IMAGE_START"], '-s', core_config["CONFIG_FDL2_IMAGE_SIZE"],
+            '-p', join(FRAMEWORK_DIR, 'lib', 'rda8910', 'fdl2.img'),
+    ]
+
+    pac_nvitem = [
+        'cfg-nvitem', '-n', '"Calibration"', '-i', '0xFFFFFFFF', '--use', '1', '--replace', '0', '--continue', '0', '--backup', '1',
+        'cfg-nvitem', '-n', '"GSM Calibration"', '-i', '0x26d', '--use', '1', '--replace', '0', '--continue', '1', '--backup', '1',
+        'cfg-nvitem', '-n', '"LTE Calibration"', '-i', '0x26e', '--use', '1', '--replace', '0', '--continue', '0', '--backup', '1',
+        'cfg-nvitem', '-n', '"IMEI"', '-i', '0xFFFFFFFF', '--use', '1', '--replace', '0', '--continue', '0', '--backup', '1',
+        'cfg-nvitem', '-n', '"BT_Config"', '-i', '0x191', '--use', '1', '--replace', '0', '--continue', '1', '--backup', '1',
+        'cfg-nvitem', '-n', '"Customer"', '-i', '0x27e', '--use', '1', '--replace', '0', '--continue', '1', '--backup', '1',
+    ]
+
+    pac_app = [
+        'cfg-image', '-i', 'APPIMG', '-a', core_config["CONFIG_APPIMG_FLASH_ADDRESS"], '-s', core_config["CONFIG_APPIMG_FLASH_SIZE"],
+		    '-p', ' "$BUILD_DIR/$PROGNAME"' + '.img'
+    ]
+
+    pac_cmd = [
+        "$PACGEN",
+        " ".join(init_fdl),
+        " ".join(pac_nvitem),
+        " ".join(pac_app),
+        "pac-gen", target_firm.get_abspath()
+    ]
+    env.Execute(env.VerboseAction(" ".join(pac_cmd), " "))
+
+# Setup ENV
+env.Append(
+    ASFLAGS=["-x", "assembler-with-cpp"],
+
+    CCFLAGS=[
+        "-Os",  # optimize for size
+        "-g",
+        "-fmessage-length=0",
+        "-ffunction-sections",  # place each function in its own section
+        "-fdata-sections",
+        "-fsigned-char",
+        "-fno-strict-aliasing",
+        "-Wall",
+        "-mthumb",
+        "-mthumb-interwork",
+        "-mcpu=cortex-a5",
+        "-mtune=generic-armv7-a",
+        "-mfpu=neon-vfpv4",
+        "-mfloat-abi=hard",
+        "-mno-unaligned-access",
+    ],
+
+    CFLAGS=[
+        "-std=gnu11"
+    ],
+
+    CXXFLAGS=[
+        "-std=gnu++11",
+        "-fno-rtti",
+        "-fno-exceptions",
+        "-fno-use-cxa-atexit",
+        "-fno-threadsafe-statics",
+    ],
+
+    CPPDEFINES=[
+        ("__BUFSIZ__", "512"),
+        ("__FILENAME_MAX__", "256")
+    ],
+
+    CPPPATH=[
+        join(FRAMEWORK_DIR, "include"),
+        join(FRAMEWORK_DIR, "include", "ril")
+    ],
+
+    LINKFLAGS=[
+        "-mthumb",
+        "-mthumb-interwork",
+        "-mcpu=cortex-a5",
+        "-mtune=generic-armv7-a",
+        "-mfpu=neon-vfpv4",
+        "-mfloat-abi=hard",
+        "-mno-unaligned-access",
+        "-Os",
+        "-Wl,--gc-sections,--relax",
+        "-nostartfiles",
+        "-nostdlib",
+        "-nostartfiles",
+        "-nodefaultlibs",
+        "-Wl,--defsym,platform_init=platform_%s_init" % board.get("build.variant")
+    ],
+
+    LIBS=["logicrom4g", "c", "gcc", "m"],
+
+    LIBPATH=[
+        join(FRAMEWORK_DIR, "lib")
+    ],
+
+    LIBSOURCE_DIRS=[join(FRAMEWORK_DIR, "libraries")],
+
+    BUILDERS=dict(
+        ElfToBin=Builder(
+            action=env.VerboseAction(gen_pac_file, "Generating $TARGET"),
+            suffix=".pac"
+        ),
+        BinToFOTA=Builder(
+            action=env.VerboseAction(" ".join([
+                '$FOTAGEN',
+                "--single-pac",
+                "$SOURCES," + join(FRAMEWORK_DIR, 'lib', 'rda8910', 'fota8910.xml'),
+                "$TARGET"
+            ]), "Generating FOTA firmware $TARGET"),
+            suffix=".bin"
+        )
+    )
+)
+
+# uploader flag update
+env.Prepend(
+    UPLOAD_EXTRA_ARGS=[
+        core_config["CONFIG_FDL1_IMAGE_START"],
+        join(FRAMEWORK_DIR, "lib", "rda8910", "fdl1.img"),
+        core_config["CONFIG_FDL2_IMAGE_START"],
+        join(FRAMEWORK_DIR, "lib", "rda8910", "fdl2.img"),
+        core_config["CONFIG_APPIMG_FLASH_ADDRESS"],
+    ]
+)
+
+# copy CCFLAGS to ASFLAGS (-x assembler-with-cpp mode)
+env.Append(ASFLAGS=env.get("CCFLAGS", [])[:])
+
+def load_logicrom_debug():
+    for i, libs in enumerate(env["LIBS"]):
+        if libs.startswith("logicrom"):
+            env["LIBS"][i] = libs + "_debug"
+
+if board.get("build.logicromtype") == "debug":
+    load_logicrom_debug()
+
+if env.GetBuildType() == "debug":
+    load_logicrom_debug()
--- builder/main.py
+++ builder/main.py
@@ -3,14 +3,11 @@
 # SPDX-License-Identifier: MIT
 #
 
-import sys
-from platform import system
 from platformio.util import get_systype
-from os import makedirs
-from os.path import basename, isdir, join
+from os.path import join
 
-from SCons.Script import (ARGUMENTS, COMMAND_LINE_TARGETS, AlwaysBuild,
-                          Builder, Default, DefaultEnvironment)
+from SCons.Script import (COMMAND_LINE_TARGETS, AlwaysBuild,
+                          Default, DefaultEnvironment)
 
 env = DefaultEnvironment()
 platform = env.PioPlatform()
@@ -34,16 +31,17 @@ env.Replace(
 
     ARFLAGS=["rc"],
 
-    SIZEPROGREGEXP=r"^(?:\.text|\.data|\.rodata|\.text.align|\.ARM.exidx|\.ARM.extab|\.ll|\.initdata)\s+(\d+).*",
-    SIZEDATAREGEXP=r"^(?:\.data|\.bss|\.noinit)\s+(\d+).*",
+    SIZEPROGREGEXP=r"^(?:\.text|\.data|\.rodata|\.text.align|\.ARM.exidx|\.ARM.extab|\.ll|\.initdata|\.init_array|\.corestub)\s+(\d+).*",
+    SIZEDATAREGEXP=r"^(?:\.data|\.bss|\.noinit|\.corestub)\s+(\d+).*",
     SIZECHECKCMD="$SIZETOOL -A -d $SOURCES",
     SIZEPRINTCMD='$SIZETOOL -B -d $SOURCES',
 
-    PROGSUFFIX=".elf"
+    PROGSUFFIX=".elf",
+    TARGETSUFFIX=".bin"
 )
 
 # Setup tools based on system type
-if "windows" in get_systype() and board.get("build.mcu") != "MT2625":
+if "windows" in get_systype() and board.get("build.mcu") in ["MT6261", "MT2503"]:
     env.Replace(
         LOGICROM_FLASHER=join(flasher_path, "logicromflasher"),
         REFLASH_FLAGS=[
@@ -67,16 +65,25 @@ if env.get("PROGNAME", "program") == "program":
 # Target: Build executable and linkable firmware
 #
 
+if board.get("build.mcu") == "RDA8910":
+    env.Replace(TARGETSUFFIX=".pac")
+
 target_elf = None
 if "nobuild" in COMMAND_LINE_TARGETS:
     target_elf = join("$BUILD_DIR", "${PROGNAME}.elf")
-    target_firm = join("$BUILD_DIR", "${PROGNAME}.bin")
-    target_firm_fota = join("$BUILD_DIR", "fota_${PROGNAME}.bin")
+    target_firm = join("$BUILD_DIR", "${PROGNAME}.${TARGETSUFFIX}")
+    target_firm_fota = join("$BUILD_DIR", "fota_${PROGNAME}.${TARGETSUFFIX}")
+    target_upload = target_firm
 else:
     target_elf = env.BuildProgram()
     target_firm = env.ElfToBin(join("$BUILD_DIR", "${PROGNAME}"), target_elf)
     target_firm_fota = env.BinToFOTA(
         join("$BUILD_DIR", "fota_${PROGNAME}"), target_firm)
+    target_upload = target_firm
+
+if board.get("build.mcu") == "RDA8910":
+    target_upload = join("$BUILD_DIR", "${PROGNAME}.img")
+    env.Depends(target_upload, target_firm)
 
 AlwaysBuild(env.Alias("nobuild", target_firm))
 target_buildprog = env.Alias("buildprog", target_firm, target_firm)
@@ -97,12 +104,14 @@ target_size = env.Alias("size", target_elf, env.VerboseAction(
 AlwaysBuild(target_size)
 
 #
-# Target: Reflash core
+# Target: Reflash core for GSM devices
 #
-AlwaysBuild(
-    env.Alias("reflash", None, [
-        env.VerboseAction("$REFLASH_CMD", "Reflashing core...")
-    ]))
+if board.get("build.mcu") in ["MT6261", "MT2503"]:
+    AlwaysBuild(
+        env.AddCustomTarget("reflash", None, [
+            env.VerboseAction("$REFLASH_CMD", "Reflashing core...")
+        ], title="Reflash Core Firmware",
+            description="Reflash module core firmware (Only available on windows)"))
 
 #
 # Target: Upload by default .bin file
@@ -115,28 +124,29 @@ env.Replace(
     ],
     UPLOADCMD='$UPLOADER $UPLOADERFLAGS $SOURCE'
 )
-upload_source = target_firm
+
+for args in env.get("UPLOAD_EXTRA_ARGS", []):
+    env.Append(UPLOADERFLAGS=[args])
+
+upload_source = target_upload
 upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
 
-if "windows" not in get_systype() or board.get("build.mcu") == "MT2625":
-    env.Append(
+if board.get("build.mcu") not in ["MT6261", "MT2503"]:
+    env.Prepend(
         UPLOADERFLAGS=[
             "-m", '"${__get_board_mcu()}"',
         ]
     )
 
-if "windows" in get_systype() and board.get("build.mcu") != "MT2625" and env.subst("$UPLOAD_PORT") == "":
-    env.Append(
-        UPLOADERFLAGS=[
-            "-u"
-        ],
-        REFLASH_FLAGS=[
-            "-u"
-        ]
-    )
-else:
-    upload_actions.insert(0, env.VerboseAction(env.AutodetectUploadPort,
-        "Looking for upload port..."))
+if env.subst("$UPLOAD_PORT") == "":
+    if "windows" in get_systype() and board.get("build.mcu") in ["MT6261", "MT2503"]:
+        env.Prepend(
+            UPLOADERFLAGS=["-u"],
+            REFLASH_FLAGS=["-u"],
+        )
+    elif board.get("build.mcu") != "RDA8910":
+        upload_actions.insert(0, env.VerboseAction(env.AutodetectUploadPort,
+            "Looking for upload port..."))
 
 AlwaysBuild(env.Alias("upload", upload_source, upload_actions))
 
